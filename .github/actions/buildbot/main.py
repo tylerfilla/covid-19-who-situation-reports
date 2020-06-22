2ee0e3bde4aca363b1e1c662a0ee4f6a3dbf822c
Tyler Filla <tylerfilla@hotmail.com>
2020-06-21 21:18:15 -0500

    Decrypt the keys

--- .github/actions/buildbot/main.py
+++ .github/actions/buildbot/main.py
@@ -2,9 +2,20 @@
 # Sitrep Scraper - Buildbot
 #
 
+import os
+
+from cryptography.fernet import Fernet
+
 
 def main():
-    print('Hello, Buildbot!')
+    # Get the encryption key
+    enckey = os.environ['INPUT_KEY']
+
+    # Decrypt the app key
+    with open('key.pem.fernet', 'rb') as file:
+        appkey = Fernet(enckey.encode()).decrypt(file.read()).decode()
+
+    print(appkey[:32])
 
 
 if __name__ == '__main__':
--- .github/actions/scrapebot/main.py
+++ .github/actions/scrapebot/main.py
@@ -2,9 +2,20 @@
 # Sitrep Scraper - Scrapebot
 #
 
+import os
+
+from cryptography.fernet import Fernet
+
 
 def main():
-    print('Hello, Scrapebot!')
+    # Get the encryption key
+    enckey = os.environ['INPUT_KEY']
+
+    # Decrypt the app key
+    with open('key.pem.fernet', 'rb') as file:
+        appkey = Fernet(enckey.encode()).decrypt(file.read()).decode()
+
+    print(appkey[:32])
 
 
 if __name__ == '__main__':
